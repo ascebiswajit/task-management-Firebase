b4c6a786805c601091658dbb36ed8ed1f44c1d73
Biswajit <ascebiswajit@gmail.com>
2024-06-16 03:55:22 +0530

    making the first screen in RN+FB

---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+// screens/HomeScreen.js
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+export default HomeScreen;
